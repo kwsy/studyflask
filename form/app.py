2070d9ee577c20aac564b5bd0e4b5cb57a3e8a28
张东升 <zhangdongsheng02@finupgroup.com>
2019-10-11 15:46:42 +0800

    表单与文件上传

---
+++ form/app.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, request, render_template, redirect
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+UPLOAD_FOLDER = './upload'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def do_login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        name = request.form['name']
+        password = request.form['password']
+        if name == 'python':
+            return render_template('index.html', name=name)
+        else:
+            return redirect('/login')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploadfile', methods=['POST', 'GET'])
+def do_upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+    return render_template('upload.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
