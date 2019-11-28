adf683db5c6b7c42ad23516d44de08fe509ab05f
张东升 <zhangdongsheng02@finupgroup.com>
2019-11-28 18:03:39 +0800

    modify

--- flask_template/app.py
+++ flask_template/app.py
@@ -12,5 +12,17 @@ def name(name):
 def test_if(name):
     return render_template('if.html', name=name)
 
+
+@app.route('/stu')
+def stu():
+    stu_lst = [
+        {'name': '小明', 'age': 14, 'score': 98},
+        {'name': '小刚', 'age': 13, 'score': 95},
+        {'name': '小红', 'age': 15, 'score': 96}
+    ]
+
+    return render_template('stu.html', stu_lst=stu_lst)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
